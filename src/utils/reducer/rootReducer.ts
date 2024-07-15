94eb447ef9299320a575d1117f91527c2cb37a35
Supansa Tantulset <supansa.tantulset@gmail.com>
2024-07-16 03:04:39 +0700

    add user reducer

--- src/utils/reducer/rootReducer.ts
+++ src/utils/reducer/rootReducer.ts
@@ -1,21 +1,25 @@
 import { PostAction, PostInitialState, PostState, postReducer } from "./postReducer";
 import { ToastAction, ToastInitialState, ToastState, toastReducer } from "./toastReducer";
+import { UserAction, UserInitialState, UserState, userReducer } from "./userReducer";
 
 export type RootState = {
   post: PostState;
   toast: ToastState;
+  user: UserState;
 };
 
-export type RootAction = PostAction | ToastAction;
+export type RootAction = PostAction | ToastAction | UserAction;
 
 export const initialState: RootState = {
   post: PostInitialState,
   toast: ToastInitialState,
+  user: UserInitialState,
 };
 
 export const rootReducer = (state: RootState, action: RootAction): RootState => {
   return {
     post: postReducer(state.post, action as PostAction),
     toast: toastReducer(state.toast, action as ToastAction),
+    user: userReducer(state.user, action as UserAction),
   };
 };
